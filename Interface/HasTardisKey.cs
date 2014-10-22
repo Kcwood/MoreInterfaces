148a6eb869c256e647ebf500ef7726f5675d34eb
Kyle Wood <kyle.c.wood88@gmail.com>
2014-10-22 11:26:58 -0600

    MoreInterfaces
    A very basic interface that I coded.

---
+++ Interface/HasTardisKey.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    public class HasTardisKey : IDoctor
+    {
+        public bool isDoctor()
+        {
+            Console.WriteLine("He has the key, he is the Doctor!");
+            return true;
+        }
+
+        public bool isNotDoctor()
+        {
+            Console.WriteLine("He can't be trusted, run!");
+            return false;
+        }
+    }
+}
